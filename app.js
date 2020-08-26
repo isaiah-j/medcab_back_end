a4a4bf4dae44b4e39adfae420608770bf973ecf5
Isaiah <greatpup1940@gmail.com>
2020-08-25 21:26:58 -0400

    handled unfounded routes in app.js

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ app.use(express.json());
 
 
 //Routes
-app.use('/api/v1/ailments',ailmentRouter)
+app.use('/api/v1/ailments', ailmentRouter)
 
-
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 404,
+        message: `Cannot find ${req.originalUrl}`
+    })
+})
 
 
 
