17beb296d6c2845eb9d98dfc5221cc784348ba5b
Isaiah <greatpup1940@gmail.com>
2020-08-25 23:50:45 -0400

    Created user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -9,12 +9,17 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, "I need your username"],
-        unique: true
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
     },
     password: {
         required: [true, 'Password is required']
     },
-    confirmPassword: [true, "Please confirm your password"]
+    confirmPassword: {
+        type: String,
+        required: [true, "Please confirm your password"]
+    }
     // other fields
 })
 
